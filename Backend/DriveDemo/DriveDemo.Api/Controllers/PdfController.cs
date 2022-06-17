2d8910c6863fc42cd2b3077d762f91a9a2ba901b
raymondhuy177 <raymondhuy177@gmail.com>
2022-06-17 22:06:02 +0700

    add backend

---
+++ Backend/DriveDemo/DriveDemo.Api/Controllers/PdfController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace DriveDemo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PdfController : ControllerBase
+    {
+        [HttpGet("info")]
+        public PdfInfo GetPdfInfo()
+        {
+            return new PdfInfo()
+            {
+                Name = "pdf",
+                NumberOfPages = 3
+            };
+        }
+
+        [HttpGet("{pageNumber}")]
+        public FileContentResult DownloadContent(int pageNumber)
+        {
+            var pdf = System.IO.File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "Pdfs", $"pdf_{pageNumber}.pdf"));
+            return new FileContentResult(pdf, "application/pdf");
+        }
+    }
+
+    public class PdfInfo
+    {
+        public string Name { get; set; }
+
+        public int NumberOfPages { get; set; }
+    }
+}
